LineageOS/android_device_samsung_sm8250-common

sm8250-common: init: Include recovery script as prebuilt_etc module
Change-Id: I07556e691d2a3b1c465f8bd8e59b3bffb391cfd3

common.mk
@@ -44,6 +44,7 @@ PRODUCT_PACKAGES += \
     init.nfc.samsung.rc \
     init.qcom.rc \
     init.ramplus.rc \
+    init.recovery.qcom.rc \
     init.samsung.bsp.rc \
     init.samsung.display.rc \
     init.samsung.eif.rc \
@@ -380,10 +381,6 @@ PRODUCT_PACKAGES += \
     libsqlite.vendor \
     libvndfwk_detect_jni.qti \
     libvndfwk_detect_jni.qti.vendor
-
-# Recovery
-PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/recovery/root/init.recovery.qcom.rc:root/init.recovery.qcom.rc
 
 # RIL
 PRODUCT_PACKAGES += \

LMODroid-Devices/device_samsung_sm8250-common

common.mk
@@ -43,6 +43,7 @@ PRODUCT_PACKAGES += \
     init.fingerprint.rc \
     init.nfc.samsung.rc \
     init.qcom.rc \
+    init.qcom.recovery.rc \
     init.ramplus.rc \
     init.samsung.bsp.rc \
     init.samsung.display.rc \
@@ -380,10 +381,6 @@ PRODUCT_PACKAGES += \
     libsqlite.vendor \
     libvndfwk_detect_jni.qti \
     libvndfwk_detect_jni.qti.vendor
-
-# Recovery
-PRODUCT_COPY_FILES += \
-    $(COMMON_PATH)/recovery/root/init.recovery.qcom.rc:root/init.recovery.qcom.rc
 
 # RIL
 PRODUCT_PACKAGES += \